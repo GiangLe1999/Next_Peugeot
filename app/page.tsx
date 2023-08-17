7b3bd8b820191bf0155f2d32012628ad8b4f3b95
GiangLe1999 <legiangbmt09@gmail.com>
2023-08-17 20:30:39 +0700

    Move getAllCatesData function to Home page component

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,17 @@ import GeneralInfo from "@/components/home/GeneralInfo";
 import HomeSwiper from "@/components/home/HomeSwiper";
 import Motorcyle from "@/components/home/Motorcyle";
 import News from "@/components/home/News";
-import { getAllCatesData } from "@/lib/fetchData";
 import { CategoryType } from "@/types";
+
+async function getAllCatesData() {
+  const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/category`);
+
+  if (!res.ok) {
+    throw new Error("Failed to fetch data");
+  }
+
+  return res.json();
+}
 
 export default async function Home() {
   const categories = (await getAllCatesData()) as CategoryType[];
