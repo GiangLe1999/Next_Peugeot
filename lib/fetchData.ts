8b1d302418e58f00b642c163a7cb35186f4e11a8
GiangLe1999 <legiangbmt09@gmail.com>
2023-08-17 22:21:35 +0700

    can understand why so disspointed

--- lib/fetchData.ts
+++ lib/fetchData.ts
@@ -1,6 +1,8 @@
 export const getAllCatesData = async () => {
   try {
-    const res = await fetch(`${process.env.NEXT_PUBLIC_BASE_URL}/api/category`);
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_BASE_URL as string}/api/category`
+    );
 
     if (!res.ok) {
       throw new Error("Failed to fetch data");
