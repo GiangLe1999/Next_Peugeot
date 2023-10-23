175ca39e3906a93dcfa95e933e6b3da50758f70c
GiangLe1999 <legiangbmt09@gmail.com>
2023-10-23 19:28:08 +0700

    Fix header

--- data/headerItems.ts
+++ data/headerItems.ts
@@ -4,7 +4,6 @@ export const headerItems = [
   { title: "NEW PEUGEOT 3008", link: "sanpham/peugeot-3008" },
   { title: "NEW PEUGEOT 5008", link: "sanpham/peugeot-5008" },
   { title: "PEUGEOT TRAVELLER", link: "sanpham/peugeot-traveller" },
-  { title: "DJANGO 125 ABS", link: "sanpham/peugeot-django" },
   { title: "TIN TỨC", link: "tin-tuc-su-kien" },
   { title: "LIÊN HỆ", link: "lien-he" },
 ];
